50c7b94777c32e65c3e2fc8c0502a9b0439cfcdf
chuxmykel <ngwobiachukwudi@gmail.com>
2019-03-22 00:21:34 +0100

    feat(UI): User dashboard design UI for user dashboard work on tabs for other features such as user transactions and accounts point sign-in and sign-up pages to the user-dashboard.html page [Delivers #164742830]

--- UI/js/main.js
+++ UI/js/main.js
@@ -3,18 +3,38 @@ const page = document.getElementById("page");
 const sideBar = document.getElementById("mySidebar");
 let open = 0;
 
-hamburger.addEventListener('click', openNav);
-function openNav() {
+const openNav = () => {
 	if (open === 1) {
 		open = 0;
 		return closeNav();
 	} 
-	sideBar.style.width = "100px";
-	page.style.marginRight = "100px";
+	sideBar.style.width = "120px";
+	page.style.marginRight = "120px";
 	open = 1;
 }
   
-function closeNav() {
+const closeNav = () => {
 	sideBar.style.width = "0";
 	page.style.marginRight= "0";
 }
+
+const displayInfo = (evt, tabName) => {
+	let i;
+	const tabcontent = document.getElementsByClassName("tabcontent");
+	const tablinks = document.getElementsByClassName("tablinks");
+	
+	for (i = 0; i < tabcontent.length; i++) {
+	  tabcontent[i].style.display = "none";
+	}
+	
+	for (i = 0; i < tablinks.length; i++) {
+	  tablinks[i].className = tablinks[i].className.replace(" active", "");
+	}
+  
+	document.getElementById(tabName).style.display = "block";
+	evt.currentTarget.className += " active";
+}
+
+hamburger.addEventListener('click', openNav);
+document.getElementById('profile-tab').style.display = "block";
+document.getElementById('profile').className += " active";
