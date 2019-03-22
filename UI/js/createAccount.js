00d92885275d6b4ea957e1acfcb0b956c0584346
chuxmykel <ngwobiachukwudi@gmail.com>
2019-03-22 03:51:23 +0100

    feat(UI): User create account - work on the accounts tab in user profile page - create js file to power modal for account creation [Delivers #164742610]

---
+++ UI/js/createAccount.js
@@ -0,0 +1,17 @@
+const modal = document.getElementById('myModal');
+const btn = document.getElementById('create');
+const close = document.getElementsByClassName('close')[0];
+
+btn.onclick = () => {
+  modal.style.display = "block";
+}
+
+close.onclick = () => {
+  modal.style.display = "none";
+}
+
+window.onclick = (event) => {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
--- UI/js/main.js
+++ UI/js/main.js
@@ -1,6 +1,6 @@
 const hamburger = document.getElementById('hamburger-menu');
-const page = document.getElementById("page");
-const sideBar = document.getElementById("mySidebar");
+const page = document.getElementById('page');
+const sideBar = document.getElementById('mySidebar');
 let open = 0;
 
 const openNav = () => {
@@ -8,33 +8,33 @@ const openNav = () => {
 		open = 0;
 		return closeNav();
 	} 
-	sideBar.style.width = "120px";
-	page.style.marginRight = "120px";
+	sideBar.style.width = '120px';
+	page.style.marginRight = '120px';
 	open = 1;
 }
   
 const closeNav = () => {
-	sideBar.style.width = "0";
-	page.style.marginRight= "0";
+	sideBar.style.width = '0';
+	page.style.marginRight= '0';
 }
 
 const displayInfo = (evt, tabName) => {
 	let i;
-	const tabcontent = document.getElementsByClassName("tabcontent");
-	const tablinks = document.getElementsByClassName("tablinks");
+	const tabcontent = document.getElementsByClassName('tabcontent');
+	const tablinks = document.getElementsByClassName('tablinks');
 	
 	for (i = 0; i < tabcontent.length; i++) {
-	  tabcontent[i].style.display = "none";
+	  tabcontent[i].style.display = 'none';
 	}
 	
 	for (i = 0; i < tablinks.length; i++) {
-	  tablinks[i].className = tablinks[i].className.replace(" active", "");
+	  tablinks[i].className = tablinks[i].className.replace(' active', '');
 	}
   
-	document.getElementById(tabName).style.display = "block";
-	evt.currentTarget.className += " active";
+	document.getElementById(tabName).style.display = 'block';
+	evt.currentTarget.className += ' active';
 }
 
 hamburger.addEventListener('click', openNav);
-document.getElementById('profile-tab').style.display = "block";
-document.getElementById('profile').className += " active";
+document.getElementById('profile-tab').style.display = 'block';
+document.getElementById('profile').className += ' active';
